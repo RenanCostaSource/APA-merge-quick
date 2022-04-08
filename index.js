70a40d77fc7a9f94a7a77a58a87bbbc12f07a136
renan-paiva <renanpaiva@fuze.cc>
2022-04-08 00:01:51 -0300

    quicksort

---
+++ index.js
@@ -0,0 +1,82 @@
+const  fs =  require('fs');
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+const algorithm = 0; // change this if you want to change the sort type 0 = quick | 1 = merge
+
+//Just change the name of the file to change the test case
+const file = fs.readFileSync('couting.txt', 'utf-8');
+const array = file.split('\r\n').map(it=>{
+    return Number.parseInt(it);
+}).filter(it =>{
+    return it !== undefined;
+});
+array.shift()
+let index = 0;
+const quickSort = function quickSort (arrayInit, arrayEnd) {
+    index++;
+   if(arrayInit<arrayEnd){
+        let q = partition(arrayInit, arrayEnd);
+        quickSort(arrayInit, q-1);
+        quickSort(q+1, arrayEnd);
+   }
+
+   return array;
+
+}
+const partition = (arrayInit, arrayEnd) => {
+    console.log(array.slice(arrayInit, arrayEnd))
+    let x = array[arrayInit];
+    let a = arrayInit+1;
+    let b = arrayEnd;
+    while(1){
+        while(array[b]>x){
+            b--
+        }
+        while(array[a]<x){
+            a++
+        }
+        if(a<=b){
+            if(array[a] == undefined || array[b]== undefined){
+                process.exit(1);
+            }
+            let aux = array[a]
+            array[a] = array[b]
+            array[b]= aux
+        } else {
+            if(array[arrayInit] == undefined || array[b]== undefined){
+                process.exit(1);
+            }
+            let aux = array[arrayInit]
+            array[arrayInit] = array[b]
+            array[b]= aux
+            return b
+        }
+    }
+
+}
+
+const timeStart = (new Date).getMilliseconds();
+const sorted = algorithm===0 
+    ? quickSort(0, array.length-1).filter(it =>{
+        return it !== undefined;
+    })
+    : []; 
+const timeEnd = (new Date).getMilliseconds();
+//validates
+let worked = true;
+for(let i = 0; i< sorted.length-1; i++){
+    if(sorted[i] > sorted[i+1]){
+        console.log(sorted[i]+">"+ sorted[i+1] );
+        worked = false;
+    }
+}
+console.log(index);
+console.log("it worked? "+ worked);
+console.log("time spent:"+ (timeEnd-timeStart));
+
+
+
+process.exit(1);
